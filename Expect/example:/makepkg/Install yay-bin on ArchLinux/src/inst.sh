8832f56db76d7f5d6030bec4fb8a2cd048d9a6aa
DarkStar <pegasus@quantasource.dev>
2025-06-05 17:34:42 -0500

    try

--- Expect/example:/makepkg/Install yay-bin on ArchLinux/src/inst.sh
+++ Expect/example:/makepkg/Install yay-bin on ArchLinux/src/inst.sh
@@ -1,7 +1,8 @@
 # https://github.com/Jguer/yay#Installation
 
-repo=
-this=automate.exp
+repo="https://raw.githubusercontent.com/Unicorn-OS/Bash-Automate-script-Interactive-Prompts"
+path="refs/heads/main/Expect/example%3A/makepkg/Install%20yay-bin%20on%20ArchLinux/src"
+this="automate.exp"
 
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
@@ -9,5 +10,5 @@ cd yay-bin
 #makepkg -si
 
 # expect script:
-wget $repo$this
+wget $repo/$path/$this
 automate.exp
